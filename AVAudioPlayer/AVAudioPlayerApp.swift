1fe092d868ddddffa041e99995f11854170ab10a
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2022-11-28 15:56:42 +0900

    first commit

---
+++ AVAudioPlayer/AVAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVAudioPlayerApp.swift
+//  AVAudioPlayer
+//
+//  Created by kotomi tahkahashi on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct AVAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
